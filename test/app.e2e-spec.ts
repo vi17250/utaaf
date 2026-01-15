a03946a6763966038ff220fc95301c68096ec6e9
vi17250 <v.aubinaud@protonmail.com>
2026-01-15 10:58:45 +0100

    feat: 🎸 Add form in front-end
    The form allows you to send an URL to the server
    
    ✅ Closes: #1

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import request from "supertest";
+import { App } from "supertest/types";
+import { AppModule } from "./../src/app.module";
 
-describe('AppController (e2e)', () => {
+describe("AppController (e2e)", () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
@@ -16,35 +16,20 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
+  it("/ (GET)", async () => {
+    const response = await request(app.getHttpServer())
+      .get("/")
       .expect(200)
-      .expect('<!DOCTYPE html>\n' +
-      '<html>\n' +
-      '    <head>\n' +
-      '        <title>Le titre de ma page HTML</title>\n' +
-      '    </head>\n' +
-      '    <body>\n' +
-      '        <h1>utaaf 🐶</h1>\n' +
-      '        <p>Url To Ascii Art Format</p>\n' +
-      '    </body>\n' +
-      '</html>');
+      .expect('Content-Type', /html/);
+      expect(response.text).toContain('<form action="/" method="post">')
+      expect(response.text).toContain('<label for="url">Enter a image URL:</label>')
+      expect(response.text).toContain('<input type="text" id="url" name="fname" />')
+      expect(response.text).toContain('<input type="submit" value="Submit">')
   });
 
-  it('/other-path (GET)', () => {
+  it('/bad-path (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('<!DOCTYPE html>\n' +
-      '<html>\n' +
-      '    <head>\n' +
-      '        <title>Le titre de ma page HTML</title>\n' +
-      '    </head>\n' +
-      '    <body>\n' +
-      '        <h1>utaaf 🐶</h1>\n' +
-      '        <p>Url To Ascii Art Format</p>\n' +
-      '    </body>\n' +
-      '</html>');
+      .get('/bad-path')
+      .expect(404);
   });
 });
