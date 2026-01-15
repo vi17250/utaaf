1a12b9d14829cb8c3becfcfee7526a2f31baaf7f
vi17250 <v.aubinaud@protonmail.com>
2026-01-15 15:32:29 +0100

    fix: 🐛 incoming_value must be a URL

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Res, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Res } from "@nestjs/common";
 import { join } from "node:path";
 import { type Response } from "express";
+
+import { UrlDto } from "./url.dto";
+import { UrlValidationPipe } from "./validation.pipe";
 
 @Controller()
 export class AppController {
@@ -10,7 +13,7 @@ export class AppController {
     }
 
     @Post()
-    create(): string {
-        return "This action returns ascii format of image";
+    async create(@Body(new UrlValidationPipe()) url: UrlDto) {
+        return `This action will returns ascii format of image url: ${url.incoming_value}`;
     }
 }
