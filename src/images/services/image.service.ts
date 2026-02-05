ac8ca21c418b4d2ac63075a0d38c185a66ae35c0
vi17250 <v.aubinaud@protonmail.com>
2026-02-05 10:40:21 +0100

    fix: 🐛 image name is a uuid

--- src/images/services/image.service.ts
+++ src/images/services/image.service.ts
@@ -1,22 +1,21 @@
 import { mkdir, writeFile } from "fs/promises";
 import { join } from "path";
-
+import { v4 as uuidv4 } from "uuid";
 
 export async function FetchAndSave(url: string): Promise<void> {
     const DATA_ROOT = join(process.cwd(), "images");
     await mkdir(DATA_ROOT, { recursive: true });
 
     try {
-        // TODO: throw errors and log it explicitely server side
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error();
         }
         const buffer = await response.arrayBuffer();
         const image = Buffer.from(buffer);
+        const image_name = uuidv4();
         await writeFile(
-            // TODO: dirname must be correctely define
-            join(DATA_ROOT, "name.jpg"),
+            join(DATA_ROOT, `${image_name}.jpg`),
             image,
         );
         console.log("SAVED!");
