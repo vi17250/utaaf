938a7ff1eb1269e0d981878af754827b5c2a7110
vi17250 <v.aubinaud@protonmail.com>
2026-02-11 16:36:12 +0100

    feat: 🎸 validate file type (`png` or `jpg` expected)
    ✅ Closes: #6

--- src/images/services/image.service.ts
+++ src/images/services/image.service.ts
@@ -1,25 +1,22 @@
 import { mkdir, writeFile } from "fs/promises";
 import { join } from "path";
 import { v4 as uuidv4 } from "uuid";
+import { fileTypeValidator } from "../utils/fileTypeValidator";
 
 export async function FetchAndSave(url: string): Promise<void> {
     const DATA_ROOT = join(process.cwd(), "images");
     await mkdir(DATA_ROOT, { recursive: true });
 
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error();
-        }
-        const buffer = await response.arrayBuffer();
-        const image = Buffer.from(buffer);
-        const image_name = uuidv4();
-        await writeFile(
-            join(DATA_ROOT, `${image_name}.jpg`),
-            image,
-        );
-        console.log("SAVED!");
-    } catch {
-        throw new Error();
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Unable to fetch data ${url}`);
     }
+    const buffer = await response.arrayBuffer();
+    const image = Buffer.from(buffer);
+    const imageformat = fileTypeValidator(image);
+    const image_name = uuidv4().concat(`.${imageformat}`);
+    await writeFile(
+        join(DATA_ROOT, `${image_name}`),
+        image,
+    );
 }
---
+++ src/images/utils/fileTypeValidator.ts
@@ -0,0 +1,15 @@
+const jpgAuthorizeBuffer: Buffer = Buffer.from([0xff, 0xd8, 0xff]);
+const pngAuthorizeBuffer: Buffer = Buffer.from([0x89, 0x50, 0x4e, 0x47]);
+
+
+type AuthorizedTypes = "jpg" | "png";
+
+export function fileTypeValidator(file: Buffer): AuthorizedTypes {
+    if (file.subarray(0,4).equals(pngAuthorizeBuffer)){
+        return "png"
+    }else if (file.subarray(0,3).equals(jpgAuthorizeBuffer)){
+        return "jpg"
+    }else{
+        throw new Error("Bad image format")
+    }
+}
