79380944605a36339b71e54bc25164e9a6520770
vi17250 <v.aubinaud@protonmail.com>
2026-02-18 17:02:30 +0100

    fix: 🐛 imageService is really a `NestJs` service

--- typescript/src/app.service.ts
+++ typescript/src/app.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { type ImageResponse } from "./images/interfaces/image.interface";
-import { FetchAndSave } from "./images/services/image.service";
+import { ImageService } from "./images/services/image.service";
 
 @Injectable()
 export class AppService {
+    constructor(
+        private readonly fetcher: ImageService,
+    ) {}
 
     async create(image_url: string): Promise<ImageResponse> {
-        const imageResponse = await FetchAndSave(image_url);
+        const imageResponse = await this.fetcher.FetchAndSave(image_url);
         return imageResponse;
     }
 }
--- typescript/src/images/services/image.service.ts
+++ typescript/src/images/services/image.service.ts
@@ -1,3 +1,4 @@
+import { Injectable } from "@nestjs/common";
 import { mkdir, writeFile } from "fs/promises";
 import { join } from "path";
 import { v4 as uuidv4 } from "uuid";
@@ -5,21 +6,24 @@ import { fileTypeValidator } from "../utils/fileTypeValidator";
 
 import { type ImageResponse } from "../interfaces/image.interface";
 
-export async function FetchAndSave(url: string): Promise<ImageResponse> {
-    const DATA_ROOT = join(process.cwd(), "images");
-    await mkdir(DATA_ROOT, { recursive: true });
+@Injectable()
+export class ImageService {
+    async FetchAndSave(url: string): Promise<ImageResponse> {
+        const DATA_ROOT = join(process.cwd(), "images");
+        await mkdir(DATA_ROOT, { recursive: true });
 
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`Unable to fetch data ${url}`);
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Unable to fetch data ${url}`);
+        }
+        const buffer = await response.arrayBuffer();
+        const image = Buffer.from(buffer);
+        const imageFormat = fileTypeValidator(image);
+        const imageName = uuidv4().concat(`.${imageFormat}`);
+        await writeFile(
+            join(DATA_ROOT, `${imageName}`),
+            image,
+        );
+        return { name: imageName, buffer: image };
     }
-    const buffer = await response.arrayBuffer();
-    const image = Buffer.from(buffer);
-    const imageFormat = fileTypeValidator(image);
-    const imageName = uuidv4().concat(`.${imageFormat}`);
-    await writeFile(
-        join(DATA_ROOT, `${imageName}`),
-        image,
-    );
-    return { name: imageName, buffer: image };
 }
