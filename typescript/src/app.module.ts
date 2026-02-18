b41e0b7883809f60616ead311700dfc029982e63
vi17250 <v.aubinaud@protonmail.com>
2026-02-18 17:02:30 +0100

    fix: 🐛 imageService is really a `NestJs` service

--- typescript/src/app.module.ts
+++ typescript/src/app.module.ts
@@ -2,11 +2,11 @@ import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { RequestIdMiddleware } from "./middlewares/logger.middleware";
-
+import { ImageService } from "./images/services/image.service";
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ImageService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
