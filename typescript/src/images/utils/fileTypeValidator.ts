88a39bfe2dd01e2e393f79eed98644efe84a7d1d
vi17250 <v.aubinaud@protonmail.com>
2026-02-19 14:42:06 +0100

    fix: 🐛 fileTypeValidator returns type `Error`

--- typescript/src/images/utils/fileTypeValidator.ts
+++ typescript/src/images/utils/fileTypeValidator.ts
@@ -4,7 +4,7 @@ const pngAuthorizeBuffer: Buffer = Buffer.from([0x89, 0x50, 0x4e, 0x47]);
 
 type AuthorizedTypes = "jpg" | "png";
 
-export function fileTypeValidator(file: Buffer): AuthorizedTypes {
+export function fileTypeValidator(file: Buffer): AuthorizedTypes | Error {
     if (file.subarray(0,4).equals(pngAuthorizeBuffer)){
         return "png"
     }else if (file.subarray(0,3).equals(jpgAuthorizeBuffer)){
