1c220eccc5b22401f095dafd550fd7f64f19ac75
vi17250 <v.aubinaud@protonmail.com>
2026-02-21 11:45:48 +0100

    docs: ✏️ JsDoc for socket service

--- typescript/src/unix_socket/socket.service.ts
+++ typescript/src/unix_socket/socket.service.ts
@@ -4,6 +4,11 @@ import { Socket } from "net";
 const SOCKET_PATH = "/home/victor/nest_rust.sock";
 const HEADER_SIZE = 4;
 
+/**
+ * Service dédié à la communication avec un autre serveur via une socket unix
+ *
+ * @class SocketService
+ */
 @Injectable()
 export class SocketService {
     private readonly client = new Socket();
@@ -22,22 +27,41 @@ export class SocketService {
         });
     }
 
-    async connect(socketPath: string = SOCKET_PATH) {
+    /**
+     * Connecte le service à la socket
+     *
+     * @param socketPath le chemin de la socket
+     */
+    async connect(socketPath: string) {
         this.client.connect(socketPath);
     }
 
-    async send(payload: Buffer): Promise<string> {
+    /**
+     * Envoie le message passé en paramètre et retourne la réponse
+     * La socket est fermée après réception
+     *
+     * @param payload l'élément à envoyer via la socket
+     * @returns une chaine de caractère
+     */
+    async send(payload: Buffer): Promise<Buffer> {
         return new Promise((resolve) => {
-            this.connect();
-            this.client.write(this.payloadWithHeader(payload));
+            this.connect(SOCKET_PATH);
+            this.client.write(this.payloadWithHeader(payload, HEADER_SIZE));
             this.client.once("data", (data) => {
-                resolve(data.toString())
+                resolve(data);
             });
         });
     }
 
-    private payloadWithHeader(payload: Buffer): Buffer {
-        const header = Buffer.alloc(HEADER_SIZE);
+    /**
+     * Calcule la taille du payload passé en paramètre et retoure la concaténation des deux
+     *
+     * @param payload le buffer de données
+     * @param header_size la taille en octets du header
+     * @returns un buffer contenant le header et le payload initial
+     */
+    private payloadWithHeader(payload: Buffer, header_size: number): Buffer {
+        const header = Buffer.alloc(header_size);
         header.writeUInt32BE(payload.length, 0);
         return Buffer.concat([header, payload]);
     }
