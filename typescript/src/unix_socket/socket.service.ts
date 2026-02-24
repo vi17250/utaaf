1e1c5a4a042bb60600a5e8d54ff6104c2743bf04
vi17250 <v.aubinaud@protonmail.com>
2026-02-24 09:44:46 +0100

    fix: 🐛 throw error when the socket server is not available

--- typescript/src/unix_socket/socket.service.ts
+++ typescript/src/unix_socket/socket.service.ts
@@ -11,29 +11,35 @@ const HEADER_SIZE = 4;
  */
 @Injectable()
 export class SocketService {
-    private readonly client = new Socket();
+    private client: Socket | null = null;
 
-    constructor() {
-        this.client.on("connect", () => {
-            console.log("[🔌 UnixSocket]\t📬 Ouverture socket");
-        });
-
-        this.client.on("close", () => {
-            console.error("[🔌 UnixSocket]\t📪 Fermeture socket");
-        });
-
-        this.client.on("error", (error) => {
-            console.error("[🔌 UnixSocket]\t⚠️ Error:", error);
-        });
-    }
 
     /**
      * Connecte le service à la socket
      *
      * @param socketPath le chemin de la socket
+     * @returns `Promise<void>` une promesse résolue lorsque que la connexion a été établie
      */
-    async connect(socketPath: string) {
-        this.client.connect(socketPath);
+    async connect(socketPath: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            const socket = new Socket();
+            socket.on("connect", () => {
+                console.log("[🔌 UnixSocket]\t📬 Ouverture socket");
+                this.client = socket;
+                resolve();
+            });
+
+            socket.on("close", () => {
+                console.error("[🔌 UnixSocket]\t📪 Fermeture socket");
+            });
+
+            socket.on("error", (error) => {
+                console.error("[🔌 UnixSocket]\t⚠️ Error:", error);
+                reject(error.message);
+            });
+
+            socket.connect(socketPath);
+        });
     }
 
     /**
@@ -41,14 +47,15 @@ export class SocketService {
      * La socket est fermée après réception
      *
      * @param payload l'élément à envoyer via la socket
-     * @returns une chaine de caractère
+     * @returns `Promise<Buffer>`une promesse résolue lorsque le serveur de socket retourne des informations
      */
     async send(payload: Buffer): Promise<Buffer> {
+        await this.connect(SOCKET_PATH);
         return new Promise((resolve) => {
-            this.connect(SOCKET_PATH);
-            this.client.write(this.payloadWithHeader(payload, HEADER_SIZE));
-            this.client.once("data", (data) => {
+            this.client?.write(this.payloadWithHeader(payload, HEADER_SIZE));
+            this.client?.once("data", (data) => {
                 resolve(data);
+                this.client?.destroy()
             });
         });
     }
