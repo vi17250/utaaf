0a3931dd0a6553b5d7266a816219fd2c558bd3a2
vi17250 <v.aubinaud@protonmail.com>
2026-03-23 15:13:50 +0100

    feat: 🎸 index explains how to use it

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -10,26 +10,64 @@ export const html = `
 <head>
     <title>Le titre de ma page HTML</title>
     <link rel="icon" type="image/x-icon" href="https://docs.nestjs.com/favicon.ico/">
+    <style>
+        h3 {
+            margin-top: 50px;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>utaaf 🐣</h1>
+    <h1>Utaaf 🐣</h1>
     <h2>Url To Ascii Art Format</h2>
-    <form action="/" method="post">
+    <div>
+        <h3>How to use me:</h3>
         <p>
-            <label for="url">Enter a image URL:</label>
-            <input type="text" id="url" name="url"/>
+            <ul>
+                <li>
+                    Paste a image url* (exemple: 
+                    <a href="https://git-scm.com/images/logos/logomark-orange@2x.png" target="_blank">
+                        https://git-scm.com/images/logos/logomark-orange@2x.png)
+                    </a>
+                </li>
+                <li>
+                    Choose a scale, from the most precise to the coarsest
+                </li>
+                <li>
+                    Click on submit, it will download a text file containing the Ascii format of the image
+                </li>
+            </ul>
+            <em>*Accepted formats: jpg | png | webp</em>
         </p>
-        <p>
+    </div>
+    <div>
+    <h3>Fill out the form bellow to get started</h3>
+        <form action="/" method="post">
+            <p>
+                <label for="url">URL:</label>
+                <input type="text" id="url" name="url"/>
+            </p>
+            <p>
             <label for="resolution">Resolution (between 1 and 10)</label>
-            <input type="number" id="resolution" name="resolution" min="2" max="10" value="2"/>
-        </p>
-        <p>
-            <input type="submit" value="Submit">
-        </p>
-    </form>
+            <select name="resolution" id="resolution">
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+            </select>
+            </p>
+            <p>
+                <input type="submit" value="Submit">
+            </p>
+        </form>
+    </div>
     <footer>
-    <p>Version: <em>${packageInfo.version}</em></p>
+        <p>Version: <em>${packageInfo.version}</em></p>
     </footer>
 </body>
 
